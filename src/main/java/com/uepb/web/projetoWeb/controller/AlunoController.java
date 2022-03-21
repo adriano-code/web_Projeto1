ecd51dd95e223505af73adb7ac3ca00546d12f87
adriano-code <paulo.adriano020602@gmail.com>
2022-03-21 14:49:56 -0300

    Segundo commit - Projeto1 - Web

--- src/main/java/com/uepb/web/projetoWeb/controller/AlunoController.java
+++ src/main/java/com/uepb/web/projetoWeb/controller/AlunoController.java
@@ -1,46 +1,57 @@
 package com.uepb.web.projetoWeb.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uepb.web.projetoWeb.domain.Aluno;
+import com.uepb.web.projetoWeb.repository.AlunoRepository;
 import com.uepb.web.projetoWeb.service.AlunoService;
 
 @RestController
+@RequestMapping("/alunos")
 public class AlunoController {
 
 	@Autowired
 	private AlunoService alunoService;
 	
-	@GetMapping(value = "/buscarAlunoPorId", produces = { "application/json" })
-	public String buscarAlunoPorId() {
-		return alunoService.buscarAlunoPorId();
-	}
-	
-	@PostMapping(value = "/inserirAluno", produces = { "application/json" })
-	public String inserirAluno() {
-		return alunoService.inserirAluno();
-	}
-	
-	@GetMapping(value = "/buscarAlunos", produces = { "application/json" })
+	//@GetMapping(value = "/buscarAlunos", produces = { "application/json" })
+	@GetMapping
 	public List<Aluno> buscarAlunos() {
 		return alunoService.buscarAlunos();
 	}
 	
-	@PutMapping(value = "/atualizarAluno", produces = { "application/json" })
-	public String atualizarAluno() {
-		return alunoService.atualizarAluno();
+	//@GetMapping(value = "/buscarAlunoPorId", produces = { "application/json" })
+	@GetMapping("/{id}")
+	public Aluno buscarAlunoPorId(@PathVariable Long id) { 
+		return alunoService.buscarAlunoPorId(id);
 	}
 	
-	@DeleteMapping(value = "/deletarAluno", produces = { "application/json" })
-	public String deletarAluno() {
-		return alunoService.deletarAluno();
+	//@PostMapping(value = "/inserirAluno", produces = { "application/json" })
+	@PostMapping
+	public Aluno inserirAluno(@RequestBody Aluno aluno) {
+		return alunoService.inserirAluno(aluno);
+	}
+	
+	//@PutMapping(value = "/atualizarAluno", produces = { "application/json" })
+	@PutMapping("/{id}")
+	public Aluno atualizarAluno(@PathVariable Long id, @RequestBody Aluno aluno) {
+		return alunoService.atualizarAluno(id, aluno);
+	}
+	
+	//@DeleteMapping(value = "/deletarAluno", produces = { "application/json" })
+	@DeleteMapping("/{id}")
+	public void deletarAluno(@PathVariable Long id) {
+		alunoService.deletarAluno(id);
 	}
 
 }
--- src/main/java/com/uepb/web/projetoWeb/service/AlunoService.java
+++ src/main/java/com/uepb/web/projetoWeb/service/AlunoService.java
@@ -1,9 +1,12 @@
 package com.uepb.web.projetoWeb.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.uepb.web.projetoWeb.domain.Aluno;
 import com.uepb.web.projetoWeb.repository.AlunoRepository;
@@ -14,24 +17,40 @@ public class AlunoService {
 	@Autowired
 	private AlunoRepository alunoRepository;
 	
-	public String buscarAlunoPorId() {
-		return "AlunoService.buscarAlunoPorId";
+	public Aluno buscarAlunoPorId(Long id) {
+		return alunoRepository.findById(id).get();
 	}
 	
 	public List<Aluno> buscarAlunos() {
 		return alunoRepository.findAll();
 	}
 	
-	public String inserirAluno() {
-		return "AlunoService.inserirAluno";
+	public Aluno inserirAluno(Aluno aluno) {
+		return alunoRepository.save(aluno);
 	}
 	
-	public String atualizarAluno() {
-		return "AlunoService.atualizarAluno";
+	public Aluno atualizarAluno(Long id, Aluno aluno) {
+		Optional<Aluno> op = alunoRepository.findById(id);
+		if(op.isPresent()) {
+			Aluno obj = op.get();
+			if(aluno.getNome() != null) {
+				obj.setNome(aluno.getNome());
+			}
+			if(aluno.getMatricula() != null) {
+				obj.setMatricula(aluno.getMatricula());
+			}
+			if(aluno.getEmail() != null) {
+				obj.setEmail(aluno.getEmail());
+			}
+			return alunoRepository.save(obj);
+		}
+		return null;
 	}
 	
-	public String deletarAluno() {
-		return "AlunoService.deletarAluno";
+	public void deletarAluno(Long id) {
+		if(alunoRepository.existsById(id)) {
+			alunoRepository.deleteById(id);
+		}
 	}
 
 }
