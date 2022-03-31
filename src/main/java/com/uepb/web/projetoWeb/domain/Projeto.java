391644b6468e7bfdd61a50f7be2d83e454f744e9
adriano-code <paulo.adriano020602@gmail.com>
2022-03-31 14:59:31 -0300

    Implementação - Exercício 2 e Projeto 1

--- src/main/java/com/uepb/web/projetoWeb/domain/Projeto.java
+++ src/main/java/com/uepb/web/projetoWeb/domain/Projeto.java
@@ -1,6 +1,5 @@
 package com.uepb.web.projetoWeb.domain;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,25 +19,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "TURMAS")
-public class Turma {
-
+@Table(name = "PROJETOS")
+public class Projeto {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
-	@Column
-	private String nomeDaDisciplina;
-
-	@Column
-	private String sala;
 	
-	@ManyToOne
+	@Column
+	private String nome;
+	
+	@Column
+	private String descricao;
+	
+	@OneToOne
 	@JoinColumn(name = "professor_id")
 	private Professor professor;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "aluno_id")
 	private Aluno aluno;
-
 }
---
+++ src/main/java/com/uepb/web/projetoWeb/service/ProjetoService.java
@@ -0,0 +1,52 @@
+package com.uepb.web.projetoWeb.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uepb.web.projetoWeb.domain.Projeto;
+import com.uepb.web.projetoWeb.repository.ProjetoRepository;
+
+@Service
+public class ProjetoService {
+
+	@Autowired
+	private ProjetoRepository projetoRepository;
+
+	public Projeto buscarProjetoPorId(Long id) {
+		return projetoRepository.findById(id).get();
+	}
+
+	public List<Projeto> buscarProjetos() {
+		return projetoRepository.findAll();
+	}
+
+	public Projeto inserirProjeto(Projeto projeto) {
+		return projetoRepository.save(projeto);
+	}
+
+	public Projeto atualizarProjeto(Long id, Projeto projeto) {
+		Optional<Projeto> op = projetoRepository.findById(id);
+		if (op.isPresent()) {
+			Projeto obj = op.get();
+			if (projeto.getNome() != null) {
+				obj.setNome(projeto.getNome());
+			}
+			if (projeto.getDescricao() != null) {
+				obj.setDescricao(projeto.getDescricao());
+			}
+
+			return projetoRepository.save(obj);
+		}
+		return null;
+	}
+
+	public void deletarProjeto(Long id) {
+		if (projetoRepository.existsById(id)) {
+			projetoRepository.deleteById(id);
+		}
+	}
+
+}
